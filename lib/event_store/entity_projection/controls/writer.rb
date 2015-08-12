afbb19ab46da4d81f7b92a8f32cd18bcb7ba03df
Scott Bellware <sbellware@gmail.com>
2015-08-12 17:49:52 -0500

    Control messages can be written independently

--- lib/event_store/entity_projection/controls/writer.rb
+++ lib/event_store/entity_projection/controls/writer.rb
@@ -7,32 +7,30 @@ module EventStore
 
           writer = EventStore::Messaging::Writer.build
 
-          messages = Controls::Message.examples
-
-          writer.write messages, stream_name
+          write_first(stream_name, writer)
+          write_second(stream_name, writer)
 
           stream_name
         end
 
-        def self.write_first(stream_name)
+        def self.write_first(stream_name, writer=nil)
           message = Controls::Message.some_message
 
-          write message, stream_name
+          write message, stream_name, writer
 
           stream_name
         end
 
-        def self.write_second(stream_name)
+        def self.write_second(stream_name, writer=nil)
           message = Controls::Message.other_message
 
-          write message, stream_name
+          write message, stream_name, writer
 
           stream_name
         end
 
-        def self.write(message, stream_name)
-
-          writer = EventStore::Messaging::Writer.build
+        def self.write(message, stream_name, writer=nil)
+          writer ||= EventStore::Messaging::Writer.build
 
           writer.write message, stream_name
         end

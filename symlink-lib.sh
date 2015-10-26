6742050a123ace0cbfbd7cb2a32cac00ee923153
Scott Bellware <sbellware@gmail.com>
2015-10-26 17:34:21 -0500

    Symlink script adds the library to the common libraries dir

---
+++ symlink-lib.sh
@@ -0,0 +1,38 @@
+set -e
+
+if [ -z ${LIBRARIES_DIR+x} ]; then
+  echo "LIBRARIES_DIR must be set to the libraries directory path... exiting"
+  exit 1
+fi
+
+if [ ! -d "$LIBRARIES_DIR" ]; then
+  echo "$LIBRARIES_DIR does not exit... exiting"
+  exit 1
+fi
+
+name=entity_projection
+prefix=event_store
+full_name=$prefix/$name
+dest="$LIBRARIES_DIR/$prefix"
+
+if [ ! -d "$dest" ]; then
+  mkdir -p "$dest"
+fi
+
+echo
+echo "Symlinking $full_name"
+echo "- - -"
+
+if [ -h "$dest/$name" ]; then
+  for entry in $LIBRARIES_DIR/$full_name*; do
+    echo "- removing symlink: $entry"
+    rm $entry
+  done
+fi
+
+echo "- creating symlinks"
+ln -s $(PWD)/lib/$full_name* $dest
+
+echo "- - -"
+echo "($full_name done)"
+echo
